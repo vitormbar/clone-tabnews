a1ed8c05d970b9553266d851c2a3428831ffa5a6
vitormbar <vitormb@gmail.com>
2025-09-22 04:35:07 +0000

    Mudança de frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Esta é uma frase feliz de novo</h1>
+    return <h1>Não tenha medo da grandeza. Alguns nascem grandes, alguns alcançam a grandeza, 
+        e a outros a grandeza é imposta.</h1>
 }
 
 export default Home;
