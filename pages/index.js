e6e77f721f098aafd88f394b6ee54ba37b237409
vitormbar <vitormb@gmail.com>
2025-09-24 19:59:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    return <h1>Não tenha medo da grandeza. Alguns nascem grandes, alguns alcançam a grandeza, 
-        e a outros a grandeza é imposta.</h1>
+  return (
+    <h1>
+      Não tenha medo da grandeza. Alguns nascem grandes, alguns alcançam a
+      grandeza, e a outros a grandeza é imposta.
+    </h1>
+  );
 }
 
 export default Home;
